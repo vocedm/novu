94698d0ea0048e7977851d565d2c297273e12f39
Fernando Erd <fernando.erd@vocedm.com.br>
2023-09-11 13:51:54 -0300

    feat(api): add webhook batch e2e test

--- apps/api/src/app/execution-details/e2e/get-webhook-status.e2e.ts
+++ apps/api/src/app/execution-details/e2e/get-webhook-status.e2e.ts
@@ -1,63 +1,41 @@
-/*
- * import { ExecutionDetailsRepository, SubscriberEntity } from '@novu/dal';
- * import { ExecutionDetailsSourceEnum, ExecutionDetailsStatusEnum, StepTypeEnum } from '@novu/shared';
- * import { UserSession, SubscribersService } from '@novu/testing';
- * import axios from 'axios';
- * import { expect } from 'chai';
- */
+import { ExecutionDetailsRepository } from '@novu/dal';
+import { UserSession } from '@novu/testing';
+import { expect } from 'chai';
+import { ExecutionDetailsSourceEnum, ExecutionDetailsStatusEnum, StepTypeEnum } from '@novu/shared';
 
-// const axiosInstance = axios.create();
+describe('should get webhook details by transactionId - /executation-details/webhook (GET)', function () {
+  let session: UserSession;
+  const executionDetailsRepository: ExecutionDetailsRepository = new ExecutionDetailsRepository();
 
-/*
- * describe('Execution details - Get webhook details by transaction id - /v1/execution-details/webhook/:transactionId (GET)', function () {
- *   let session: UserSession;
- *   const executionDetailsRepository: ExecutionDetailsRepository = new ExecutionDetailsRepository();
- *   let subscriber: SubscriberEntity;
- *   let subscriberService: SubscribersService;
- */
+  beforeEach(async () => {
+    session = new UserSession();
+    await session.initialize();
+  });
 
-/*
- *   beforeEach(async () => {
- *     session = new UserSession();
- *     await session.initialize();
- *     subscriberService = new SubscribersService(session.organization._id, session.environment._id);
- *     subscriber = await subscriberService.createSubscriber();
- *   });
- */
+  it('should not allow to update the integration with same identifier', async function () {
+    const transactionId = 'transactionId';
+    const detail = await executionDetailsRepository.create({
+      _jobId: ExecutionDetailsRepository.createObjectId(),
+      _environmentId: session.environment._id,
+      _organizationId: session.organization._id,
+      _notificationId: ExecutionDetailsRepository.createObjectId(),
+      _notificationTemplateId: ExecutionDetailsRepository.createObjectId(),
+      _subscriberId: ExecutionDetailsRepository.createObjectId(),
+      providerId: '',
+      transactionId: 'transactionId',
+      channel: StepTypeEnum.EMAIL,
+      detail: '',
+      source: ExecutionDetailsSourceEnum.WEBHOOK,
+      status: ExecutionDetailsStatusEnum.SUCCESS,
+      isTest: false,
+      isRetry: false,
+    });
+    const payload = {
+      transactionId: ['transactionId'],
+    };
 
-/*
- *   it('should get webhook executation details by transactionId', async function () {
- *     const transactionId = 'transactionId';
- *     const detail = await executionDetailsRepository.create({
- *       _jobId: ExecutionDetailsRepository.createObjectId(),
- *       _environmentId: session.environment._id,
- *       _organizationId: session.organization._id,
- *       _notificationId: ExecutionDetailsRepository.createObjectId(),
- *       _notificationTemplateId: ExecutionDetailsRepository.createObjectId(),
- *       _subscriberId: subscriber._id,
- *       providerId: '',
- *       transactionId: 'transactionId',
- *       channel: StepTypeEnum.EMAIL,
- *       detail: '',
- *       source: ExecutionDetailsSourceEnum.WEBHOOK,
- *       status: ExecutionDetailsStatusEnum.SUCCESS,
- *       isTest: false,
- *       isRetry: false,
- *     });
- */
-
-/*
- *     const {
- *       data: { data },
- *     } = await axiosInstance.get(`${session.serverUrl}/v1/execution-details/webook?transactionId=${transactionId}`, {
- *       headers: {
- *         authorization: `ApiKey ${session.apiKey}`,
- *       },
- *     });
- *     const responseDetail = data[0];
- *     expect(responseDetail.transactionId).to.equal(transactionId);
- *     expect(responseDetail.channel).to.equal(detail.channel);
- *     expect(responseDetail._id).to.equal(detail._id);
- *   });
- * });
- */
+    const { body } = await session.testAgent.get(`/v1/executation-details/webhook`).send(payload);
+    console.log(body);
+    expect(body[0].transactionId).to.equal('transactionId');
+  });
+});
